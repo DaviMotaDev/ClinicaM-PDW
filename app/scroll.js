28f1afd096a5b39ce33d6a8724762c4eb19b084a
Davi <davimota92@gmail.com>
2025-04-20 21:54:45 -0300

    ultimo commit

---
+++ app/scroll.js
@@ -0,0 +1,9 @@
+document.getElementById('home').addEventListener('click', () => {
+    document.getElementById('destinoHome').scrollIntoView({behavior:"smooth"})
+})
+document.getElementById('sobre').addEventListener('click', () => {
+    document.getElementById('destinoSobre').scrollIntoView({behavior:"smooth"})
+})
+document.getElementById('serviços').addEventListener('click', () => {
+    document.getElementById('destinoServiços').scrollIntoView({ behavior:"smooth"})
+})
